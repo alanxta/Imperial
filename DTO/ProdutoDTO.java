11afea5db802b7399e0f015029d5cf8a7b466e1b
Paulo.Barreto <paulo.barreto3394@gmail.com>
2025-03-15 10:40:03 -0300

    Update ProdutoDTO.java

--- DTO/ProdutoDTO.java
+++ DTO/ProdutoDTO.java
@@ -7,7 +7,6 @@ public class Produtoduto{
     private float precoVendaProduto;
     private float margemLucroProduto;
     private int estoqueProduto;
-    private int totalAtributosProdutoxProduto;
     private String codigoDeBarrasProduto;
     private String grupoProduto;
     private String categoriaProduto;
@@ -24,6 +23,95 @@ public class Produtoduto{
 
 
 
+    public String getDescricaoProduto(){
+        return descricaoProduto;
+    }
+    public void setDescricaoProduto(String descricaoProduto){
+        this.descricaoProduto = descricaoProduto;
+    }
 
+
+ public String getCodigoProduto(){
+        return codigoProduto;
+    }
+    public void setCodigoProduto(String codicoProduto){
+        this.codigoProduto = codicoProduto;
+    }
+
+
+    public float getPrecoCompraProduto(){
+        return precoCompraProduto;
+    }
+    public void setPrecoCompraProduto(float precoCompraProduto){
+        this.precoCompraProduto = precoCompraProduto;
+    }
+
+
+
+public float getPrecoVendaProduto(){
+        return precoVendaProduto;
+    }
+    public void setPrecoVendaProduto(float precoVendaProduto){
+        this.precoVendaProduto = precoVendaProduto;
+    }
+    
+
+
+    public float getMargemLucroProduto(){
+        return margemLucroProduto;
+    }
+    public void setMargemLucroProduto(float margemLucroProduto){
+        this.margemLucroProduto = margemLucroProduto;
+    }
+
+
+    public int getEstoqueProduto(){
+        return estoqueProduto;
+    }
+    public void setEstoqueProduto(int estoqueProduto){
+        this.estoqueProduto = estoqueProduto;
+    }
+
+
+
+    public String getCodigoDeBarrasProduto(){
+        return codigoDeBarrasProduto;
+    }
+    public void setCodigoDeBarrasProduto(String codigoDeBarrasProduto){
+        this.codigoDeBarrasProduto = codigoDeBarrasProduto;
+    }
+
+
+    public String getGrupoProduto(){
+        return grupoProduto;
+    }
+    public void setGrupoProduto(String grupoProduto){
+        this.grupoProduto = grupoProduto;
+    }
+
+
+    public String getCategoriaProduto(){
+        return categoriaProduto;
+    }
+    public void setCategoriaProduto(String categoriaProduto){
+        this.categoriaProduto = categoriaProduto;
+    }
+
+
+    public String getFornecedorProduto(){
+        return fornecedorProduto;
+    }
+    public void setFornecedorProduto(String fornecedorProduto){
+        this.fornecedorProduto = fornecedorProduto;
+    }
+
+
+
+ public String getMarcaProduto(){
+        return marcaProduto;
+    }
+    public void setMarcaProduto(String marcaProduto){
+        this.marcaProduto = marcaProduto;
+    }
 
 }
