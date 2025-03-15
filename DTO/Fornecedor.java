305918f3b22fdfe4859bacde64c909f27f493728
Paulo.Barreto <paulo.barreto3394@gmail.com>
2025-03-15 10:25:46 -0300

    Add files via upload

---
+++ DTO/Fornecedor.java
@@ -0,0 +1,101 @@
+public class Fornecedor{
+
+    private int idFornecedor;
+    private String cnpjFornecedor;
+    private String nomeFantasiaFornecedor;
+    private String razaoSocialFornecedor;
+    private String cepFornecedor;
+    private String enderecoFornecedor;
+    private String telefoneFornecedor;
+    private String emailFornecedor;
+
+public int getIdFornecedor(){
+        return idFornecedor;
+    }
+    public void setIdFornecedor(int idFornecedor){
+        this.idFornecedor = idFornecedor;
+    }
+
+
+
+    public String getCnpjFornecedor(){
+        return cnpjFornecedor;
+    }
+    public void setCnpjFornecedor(String cnpjFornecedor){
+        this.cnpjFornecedor = cnpjFornecedor;
+    }
+
+
+
+
+
+    public String getNomeFantasiaFornecedor (String nomeFantasiaFornecedor){
+        return nomeFantasiaFornecedor;
+    }
+    public void setNomeFantasiaFornecedor(String nomeFantasiaFornecedor){
+        this.nomeFantasiaFornecedor = nomeFantasiaFornecedor;
+
+    }
+
+
+
+      public String getRazaoSocialFornecedor (String razaoSocialFornecedor){
+        return razaoSocialFornecedor;
+    }
+    public void setRazaoSocialFornecedor(String razaoSocialFornecedor){
+        this.razaoSocialFornecedor = razaoSocialFornecedor;
+        
+    }
+
+    
+
+         public String getCepFornecedor (String cepFornecedor){
+        return cepFornecedor;
+    }
+    public void setCepFornecedor(String cepFornecedor){
+        this.cepFornecedor = cepFornecedor;
+        
+    }
+
+
+
+
+        public String getEnderecoFornecedor (String enderecoFornecedor){
+        return enderecoFornecedor;
+    }
+    public void setEnderecoFornecedor(String enderecoFornecedor){
+        this.enderecoFornecedor = enderecoFornecedor;
+        
+    }
+
+
+
+    
+         public String getTelefoneFornecedor (String telefoneFornecedor){
+        return telefoneFornecedor;
+    }
+    public void setTelefoneFornecedor(String telefoneFornecedor){
+        this.telefoneFornecedor = telefoneFornecedor;
+    }
+
+
+
+
+  public String getEmailFornecedor (String emailFornecedor){
+        return emailFornecedor;
+    }
+    public void setEmailFornecedor(String emailFornecedor){
+        this.emailFornecedor = emailFornecedor;
+    }
+
+
+
+
+
+
+
+
+    
+    
+
+}
