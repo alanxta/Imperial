b9951fb0d0fa5a8764de1e581a9f0193f979d81a
Brendadev01 <brenda.melo.silva15@gmail.com>
2025-04-29 15:12:28 -0300

    Usuario.java

--- Model/DTO/Usuario.java
+++ Model/DTO/Usuario.java
@@ -1,3 +1,5 @@
+package DTO;
+
 public class Usuario {
 
 	private int idUsuario;
@@ -8,6 +10,21 @@ public class Usuario {
     private String enderecoUsuario;
     private String telefoneUsuario;
     private int idadeUsuario;
+
+   public Usuario(int idUsuario, String nomeUsuario, String emailUsuario, String senhaUsuario, String cpfUsuario, String enderecoUsuario, String telefoneUsuario, int idadeUsuario){
+        this.idUsuario = idUsuario;
+        this.nomeUsuario = nomeUsuario;
+        this.emailUsuario = emailUsuario;
+        this.senhaUsuario = senhaUsuario;
+        this.cpfUsuario = cpfUsuario;
+        this.enderecoUsuario = enderecoUsuario;
+        this.telefoneUsuario = telefoneUsuario;
+        this.idadeUsuario = idadeUsuario;
+    }
+
+    public Usuario() {
+
+    }
 
     public int getIdUsuario(){
         return idUsuario;
@@ -63,6 +80,18 @@ public class Usuario {
     }
     public void setIdadeUsuario(int idadeUsuario){
         this.idadeUsuario = idadeUsuario;
+    }
+
+
+    public void mostrarInfo() {
+        System.out.println("ID: " + getIdUsuario());
+        System.out.println("Nome: " + getNomeUsuario());
+        System.out.println("Email: " + getEmailUsuario());
+        System.out.println("Senha: " + getSenhaUsuario());
+        System.out.println("CPF: " + getCpfUsuario());
+        System.out.println("Endereço: " + getEnderecoUsuario());
+        System.out.println("Telefone: " + getTelefoneUsuario());
+        System.out.println("Idade: " + getIdadeUsuario());
     }
 
 }
